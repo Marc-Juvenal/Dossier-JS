49e1353d61f30d93129ee7d41222dd318082f77b
Marc Juvénal <marcjuvenal95@gmail.com>
2024-10-11 23:07:01 +0200

    Ajout de l'exercice variable

---
+++ variables.js
@@ -0,0 +1,34 @@
+
+  /*
+    🐦 DAVID Bruno Twitter profile 🐦
+
+    🏁 Please take this quick challenge and rename carefully all the variable, and fix this broken code by assigning the correct 
+    variable keyword you have learned in the quest.
+    ex: const name = "David";
+
+  */
+
+const Name ="David"
+const lastName = "Bruno"
+console.log(lastName); // Bruno
+
+
+let biography = "Hi, I'm David Bruno from SF, I like to cook and meet new people."
+
+const Davidprofilepicture = "http://www.go.com/davif.png";
+
+let city = "San Francisco"
+
+let followers = 109;
+let following = 200;
+
+
+// 🏁 Exercise 02 - David is following one more account increment the total of following account
+
+
+let followingaccount = 1;
+
+followingaccount++;
+console.log(followingaccount);
+
+
