3f70a1035f695940e436692788b2ff60d7122010
Marc Juvénal <marcjuvenal95@gmail.com>
2024-10-12 18:41:21 +0200

    Ajout de l'exercice Evaluer les variables

---
+++ exercices_conditions.js
@@ -0,0 +1,16 @@
+// Exercice évaluer les variables
+
+const x = 2;
+const y = 5;
+const z = 10;
+
+if (x === y && y === z) {
+    console/log("Les trois variables sont identiques");
+} 
+
+else if (x === y || y === z) {
+    console.log("Deux des variables sont identiques");
+}
+
+else {console.log("toutes les variables sont différentes");
+}
